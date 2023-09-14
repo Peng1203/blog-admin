5864a98efa444de39c0299c9581511dd238f0930
Peng1203 <2567810155@qq.com>
2023-09-14 19:29:32 +0800

    style: 定于预样式类

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss';
 
 export default defineConfig({
   rules: [
-    ['m-1', { margin: '1px' }],
+    [/^mg(\d+)$/, ([, d]) => ({ margin: `${d}px` })],
+    [/^pd(\d+)$/, match => ({ padding: `${match[1]}px` })],
+    [/^mt(\d+)$/, match => ({ 'margin-top': `${match[1]}px` })],
+    [/^mr(\d+)$/, match => ({ 'margin-right': `${match[1]}px` })],
+    [/^mb(\d+)$/, match => ({ 'margin-bottom': `${match[1]}px` })],
+    [/^ml(\d+)$/, match => ({ 'margin-left': `${match[1]}px` })],
+    [/^pt(\d+)$/, match => ({ 'padding-top': `${match[1]}px` })],
+    [/^pr(\d+)$/, match => ({ 'padding-right': `${match[1]}px` })],
+    [/^pb(\d+)$/, match => ({ 'padding-bottom': `${match[1]}px` })],
+    [/^pl(\d+)$/, match => ({ 'padding-left': `${match[1]}px` })],
+    [/^w([1-9]\d?|100)$/, ([, d]) => ({ width: `${d}%` })],
+    [/^h([1-9]\d?|100)$/, ([, d]) => ({ height: `${d}%` })],
     [
       'flex-c-c',
       {
@@ -11,7 +22,58 @@ export default defineConfig({
         'align-items': 'center',
       },
     ],
+    [
+      'flex-s-c',
+      {
+        display: 'flex',
+        'justify-content': 'start',
+        'align-items': 'center',
+      },
+    ],
+    [
+      'flex-e-c',
+      {
+        display: 'flex',
+        'justify-content': 'end',
+        'align-items': 'center',
+      },
+    ],
+    [
+      'flex-sa-c',
+      {
+        display: 'flex',
+        'justify-content': 'space-around',
+        'align-items': 'center',
+      },
+    ],
+    [
+      'flex-sb-c',
+      {
+        display: 'flex',
+        'justify-content': 'space-between',
+        'align-items': 'center',
+      },
+    ],
+    [
+      'flex-se-c',
+      {
+        display: 'flex',
+        'justify-content': 'space-evenly',
+        'align-items': 'center',
+      },
+    ],
+    [/^flex(\d+)$/, match => ({ padding: `flex: ${match[1]}` })],
+    // 伪光标
+    [
+      'pseudo-c-p',
+      {
+        cursor: 'pointer',
+      },
+    ],
   ],
+  shortcuts: {
+    btn: 'py-2 px-4 font-semibold rounded-lg',
+  },
   presets: [
     presetUno(),
     presetIcons(),
@@ -21,4 +83,4 @@ export default defineConfig({
       ignoreAttributes: [],
     }),
   ],
-})
+});

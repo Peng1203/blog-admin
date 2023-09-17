e134f0e63f8a2beaba16be2e5c33da0bea06f627
Peng1203 <2567810155@qq.com>
2023-09-17 20:55:52 +0800

    feat: 定义Api相关类型

---
+++ src/types/api.d.ts
@@ -0,0 +1,43 @@
+import { AxiosResponse } from 'axios';
+
+declare module 'Api' {
+  // 请求列表 通用参数
+  export type RequestListParams<T = any> = {
+    page: number;
+    pageSize: number;
+    queryStr: string;
+    column: string;
+    order: string;
+    [key: string]: T;
+  };
+
+  export interface ApiBaseResponse<T = any> {
+    /**
+     * Api状态码
+     * 20000 请求成功
+     * 20001 更新成功
+     * 20100 创建成功
+     */
+    code: number;
+    /**
+     * 请求状态
+     */
+    success: boolean;
+    /**
+     * 响应消息
+     */
+    message: string;
+    data: T;
+  }
+  /**
+   * @description 没有经过 后端拦截器转换的接口 数据结构
+   */
+  export type RawResponse<T = any> = Promise<AxiosResponse<T>>;
+
+  /**
+   * @description 接口成功 经过响应拦截器转换的 数据结构接口成功 经过响应拦截器转换的 数据结构
+   */
+  export type TransformedResponse<T> = Promise<AxiosResponse<ApiBaseResponse<T>>>;
+
+  //
+}
--- src/types/axios.d.ts
+++ src/types/axios.d.ts
@@ -13,31 +13,4 @@ declare module 'axios' {
     status: number;
     statusText: string;
   }
-  export type RequestListParams<T = any> = {
-    page: number;
-    pageSize: number;
-    queryStr: string;
-    column: string;
-    order: string;
-    [key: string]: T;
-  };
-
-  export interface ApiBaseResponse<T = any> {
-    /**
-     * Api状态码
-     * 20000 请求成功
-     * 20001 更新成功
-     * 20100 创建成功
-     */
-    code: number;
-    /**
-     * 请求状态
-     */
-    success: boolean;
-    /**
-     * 响应消息
-     */
-    message: string;
-    data: T;
-  }
 }

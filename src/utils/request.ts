4fb2a416988a444353aa899cb3fe50995908cd9a
Peng1203 <2567810155@qq.com>
2023-09-19 18:45:11 +0800

    refactor: 优化刷新token代码

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -64,21 +64,29 @@ service.interceptors.response.use(
         ElMessage.error(data.message);
         break;
       case 401:
-        if (response?.data.code === 40104) {
-          // token过期触发的401 存在2种情况 access_token过期 refresh_token过期
-          // 根据接口中的 错误信息判断
-          // 该情况为 access_token 过期 需要调用刷新token的接口
-          // 重发出401接口的请求 该接口返回一个boolean类型 标识刷新tokem接口成功或者失败  刷新失败则不从新发起请求 否则会进入死循环
-          const refreshIsSuccess = await handleRefreshACToken();
-          if (!refreshIsSuccess) return;
-          // 修改准备发送的config对象的 token字段 否则会死循环401
-          config!.headers['Authorization'] = `Bearer ${Session.getACToken}`;
-          // 重新发送请 并将该次请求返回出去 用于触发业务组件中的后续操作
-          return service.request(error.config!);
-        } else if (response?.data.code === 40105) {
-          // 该情况为 refresh_token 过期 直接返回登录页
-          ElMessage.error(data.message);
-          return router.push({ name: 'login' });
+        const apiCode = response?.data.code;
+        switch (apiCode) {
+          case 40102:
+            ElMessage.warning(data.message);
+            break;
+          case 40104:
+            // token过期触发的401 存在2种情况 access_token过期 refresh_token过期
+            // 根据接口中的 错误信息判断
+            // 该情况为 access_token 过期 需要调用刷新token的接口
+            // 重发出401接口的请求 该接口返回一个boolean类型 标识刷新tokem接口成功或者失败  刷新失败则不从新发起请求 否则会进入死循环
+            const refreshIsSuccess = await handleRefreshACToken();
+            if (!refreshIsSuccess) return;
+            // 修改准备发送的config对象的 token字段 否则会死循环401
+            config!.headers['Authorization'] = `Bearer ${Session.getACToken}`;
+            // 重新发送请 并将该次请求返回出去 用于触发业务组件中的后续操作
+            return service.request(error.config!);
+          case 40105:
+            // 该情况为 refresh_token 过期 直接返回登录页
+            ElMessage.error(data.message);
+            return router.push({ name: 'login' });
+          default:
+            ElMessage.error(data.message);
+            break;
         }
         break;
       default:

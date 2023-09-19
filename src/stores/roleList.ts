0982d26e30357249be98701b898654026fe8edc1
Peng1203 <2567810155@qq.com>
2023-09-19 18:50:47 +0800

    feat: 补充RoleStore

--- src/stores/roleList.ts
+++ src/stores/roleList.ts
@@ -1,26 +1,32 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { useRoleApi } from '@/api';
+import { RoleData, RoleListData } from '@/views/user/role';
 
 const { getRole } = useRoleApi();
 
 export const useRolesInfo = defineStore('rolesInfo', {
   state: () => ({
     // 角色数据列表
-    roleList: ref<RoleDate[]>([]),
+    roleList: ref<RoleData[]>([]),
     // 角色下拉数据
     roleOption: ref<OptionItem[]>([]),
   }),
   actions: {
     async getRoleData(isUpdate?: boolean) {
       try {
-        const {} = await getRole({
+        if (!isUpdate && this.roleList.length) return;
+        const { data: res } = await getRole<RoleListData>({
           page: 1,
           pageSize: 999,
           queryStr: '',
           column: '',
           order: '',
         });
+        const { code, success, data } = res;
+        if (code !== 20000 || !success) return;
+        this.roleList = data.list;
+        this.roleOption = data.list.map(({ roleName, id }) => ({ label: roleName, value: id }));
       } catch (error) {
         throw error;
       }

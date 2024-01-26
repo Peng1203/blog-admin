16391a87c8ef8008bc272f19639e4dbc4f3cf615
Peng1203 <2567810155@qq.com>
2024-01-26 19:07:34 +0800

    feat(Editor): 创建Ai富文本编辑器组件

---
+++ src/components/AiEditor/AiEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    ref="divRef"
+    style="height: 600px"
+  />
+</template>
+
+<script setup lang="ts">
+import { AiEditor } from 'aieditor';
+import 'aieditor/dist/style.css';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const divRef = ref();
+let aiEditor: AiEditor | null = null;
+
+onMounted(() => {
+  aiEditor = new AiEditor({
+    element: divRef.value as Element,
+    placeholder: '点击输入内容...',
+    content: 'AiEditor 是一个面向 AI 的开源富文本编辑器。 ',
+  });
+});
+
+onUnmounted(() => {
+  aiEditor && aiEditor.destroy();
+});
+</script>

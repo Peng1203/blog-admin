c8586326d74d8693d7367156dca302d7a11b2399
Peng1203 <2567810155@qq.com>
2024-01-26 19:08:57 +0800

    feat(Article): 写文章页面开发

---
+++ src/views/article/writeArticle/WriteArticle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Preview v-model="articleForm.content" />
+
+    <MarkdownEditor v-model="articleForm.content" />
+    <!-- <div class="table-container layout-padding">
+      <el-card
+      shadow="hover"
+      class="layout-padding-auto"
+      ></el-card>
+    </div> -->
+  </div>
+</template>
+
+<script setup lang="ts" name="WriteArticle">
+import MarkdownEditor, { Preview } from '@/components/MarkdownEditor';
+import { reactive } from 'vue';
+const articleForm = reactive({
+  content: '',
+});
+</script>
+
+<style scoped lang="scss"></style>

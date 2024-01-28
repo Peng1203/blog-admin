37b5206ed02e0f9a2aa558023005d60ab6cd4ef9
Peng1203 <2567810155@qq.com>
2024-01-28 18:47:19 +0800

    chore: 添加文章相关静态数据

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+/** 文章相关静态数据 */
+export const ARTICLE = {
+  // 文章类型筛选数据
+  typeOptions: [
+    { label: '全部', value: 0 },
+    { label: '原创', value: 1 },
+    { label: '转载', value: 2 },
+    { label: '翻译', value: 3 },
+  ],
+
+  // 文章状态筛选数据
+  statusOptions: [
+    { label: '全部', value: 0 },
+    { label: '已发布', value: 1 },
+    { label: '私密', value: 2 },
+    { label: '草稿箱', value: 3 },
+    { label: '已删除', value: 4 },
+    { label: '待审核', value: 5 },
+    { label: '已拒绝', value: 6 },
+  ],
+}

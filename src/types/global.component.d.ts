3d37aeadd629ee37217d479bfd6b61e04d132f9f
Peng1203 <2567810155@qq.com>
2024-02-01 18:44:07 +0800

    update: 全局注册组件声明类型

--- src/types/global.component.d.ts
+++ src/types/global.component.d.ts
@@ -1,15 +1,18 @@
 import Table from '@/components/Table';
 import Form from '@/components/Form';
+import Dialog from '@/components/Dialog';
 
 declare module 'vue' {
   interface GlobalComponents {
     Table: typeof Table;
     PengTable: typeof Table;
 
-    Form: typeof Form
-    PengForm: typeof Form
-    'Peng-Form': typeof Form
+    Form: typeof Form;
+    PengForm: typeof Form;
+    'Peng-Form': typeof Form;
+
+    Dialog: typeof Dialog;
+    PengDialog: typeof Dialog;
+    'Peng-Dialog': typeof Dialog;
   }
-
-
 }

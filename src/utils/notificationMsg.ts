170ef63277ef86fb9750003b5bf627bf9a1c0242
Peng1203 <2567810155@qq.com>
2024-02-06 19:42:38 +0800

    feat: 全局通知消息组件封装

--- src/utils/notificationMsg.ts
+++ src/utils/notificationMsg.ts
@@ -1,24 +1,33 @@
 import { ElNotification, ElMessage } from 'element-plus';
 
-type MsgTypeEnum = 'success' | 'Warning' | 'Info' | 'Error';
+type MsgTypeEnum = 'success' | 'warning' | 'info' | 'error';
 
-/**
- * 1 为ElNotification 通知组件
- * 2 为ElMessage 消息提示组件
- */
 type NotificationTypeEnum = 1 | 2;
 
+/**
+ * notificationType
+ *  1 为ElNotification 通知组件
+ *  2 为ElMessage 消息提示组件
+ */
 export const useNotificationMsg = (
   title: string,
   message: string,
   msgType: MsgTypeEnum = 'success',
-  notificationType: NotificationTypeEnum = 1
+  notificationType: NotificationTypeEnum = 1,
+  duration: number = 3000
 ) => {
   if (notificationType === 1) {
     ElNotification({
       title,
-      message
-      type: msgType
-    })
+      message,
+      duration,
+      type: msgType,
+    });
+  } else {
+    ElMessage({
+      duration,
+      message,
+      type: msgType,
+    });
   }
 };

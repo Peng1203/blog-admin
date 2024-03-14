9ed6c6524abd9341d8d075e244b6cde6abf621a1
Peng1203 <peng01203@gmail.com>
2024-03-14 19:24:21 +0800

    feat(Util): :sparkles: 封装Fetch Api

---
+++ src/utils/fetchRequest.ts
@@ -0,0 +1,19 @@
+import { Session } from './storage';
+
+interface FetchRequestConfig extends RequestInit {
+  url: string;
+}
+/**
+ * fetch API 封装
+ */
+const request = ({ url, ...args }: FetchRequestConfig) => {
+  return fetch(`${import.meta.env.VITE_API_URL}${url}`, {
+    ...args,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Session.getACToken()}`,
+    },
+  });
+};
+
+export default request;

83d5d45329429a495d51c65802ea187105c6cd63
Peng1203 <peng01203@gmail.com>
2024-05-08 11:19:40 +0800

    chore: Select 注册全局组件

--- src/types/globalComponent.d.ts
+++ src/types/globalComponent.d.ts
@@ -3,6 +3,7 @@ import Form from '@/components/Form';
 import Dialog from '@/components/Dialog';
 import Skeleton from '@/components/Skeleton';
 import Search from '@/components/Search';
+import Select from '@/components/Select';
 
 /** 封装Form表单的实例 */
 declare module 'MyComponent' {
@@ -12,14 +13,11 @@ declare module 'MyComponent' {
 declare module 'vue' {
   interface GlobalComponents {
     'Peng-Table': typeof Table;
-
     'Peng-Form': typeof Form;
-
     'Peng-Dialog': typeof Dialog;
-
     'Peng-Skeleton': typeof Skeleton;
-
     'Peng-Search': typeof Search;
+    'Peng-Select': typeof Select;
   }
 
   type FormInstance = InstanceType<typeof Form>;

8add70e89ae7783df0c351b55d0b990b3518f3b9
Peng1203 <peng01203@gmail.com>
2024-05-14 09:50:14 +0800

    refactor: 创建通用组合式函数

---
+++ src/composables/useComponentRef.ts
@@ -0,0 +1,8 @@
+import { ref } from 'vue';
+
+export const useComponentRef = <T extends abstract new (...args: any) => any>(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  _comp: T
+) => {
+  return ref<InstanceType<T>>();
+};
